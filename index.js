f8f1aac6920cdb0271d73d70fb935e43e36a8a77
arjunvinod004 <arjunvt004@gmail.com>
2024-08-03 18:06:23 +0530

    teresa backend

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const { log } = require('console');
 const app= express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-const { request } = require('undici');
 
 const port = process.env.PORT || 8000
 app.use(cors({
@@ -26,68 +25,72 @@ app.get('/',(req,res)=>{
 app.get('/kyc',(req,res)=>{
     const {PrmCmpId,PrmBrId}=req.body
     fetch(`https://coinoneglobal.in/teresa_trial/WebOnlineCustomer.asmx/FnGetKycNo?PrmCmpId=${req.query.PrmCmpId}&PrmBrId=${req.query.PrmBrId}`)
-    .then((responsee)=>{
-        return responsee.json()
-    })
-    .then((response)=>{res.json(response)
+    .then((response)=>
+        response.json())
+        .then((response)=>{
+        res.json(response)
       
-    }
+
+        }
 )
 })
 
 app.get('/function',(req,res)=>{
     
     fetch('https://coinoneglobal.in/teresa_trial/WebOnlineCustomer.asmx/FnGetFunctionList')
-    .then((response)=>{
-        return response.json()
-    })
-
-    .then((response)=>{
+    .then((response)=>
+        response.json())
+        .then((response)=>{
         res.json(response)
-    })
+        // console.log(response);
+
+        })
 })
 
 app.get('/material',(req,res)=>{
     const {PrmCmpId,PrmBrId}=req.body
     fetch(`https://coinoneglobal.in/teresa_trial/WebOnlineCustomer.asmx/FnGetProductDressTypeList?PrmCmpId=${req.query.PrmCmpId}&PrmBrId=${req.query.PrmBrId}`)
-    .then((responsee)=>{
-        return responsee.json()
-    })
-    .then((response)=>{
+    .then((response)=>
+        response.json())
+        .then((response)=>{
         res.json(response)
-    })
+        // console.log(response);
+
+        })
 })
 
 app.get('/country',(req,res)=>{
 fetch('https://coinoneglobal.in/teresa_trial/WebOnlineCustomer.asmx/FnGetCountryList')
-.then((response)=>{
-    return response.json()
-})
-.then((respo)=>{
-    res.json(respo)
-})
+.then((response)=>
+    response.json())
+    .then((response)=>{
+    res.json(response)
+   
+
+    })
 
 })
 
 app.get('/reference',(req,res)=>{
     fetch('https://coinoneglobal.in/teresa_trial/WebOnlineCustomer.asmx/FnGetReffernceList')
-    .then((response)=>{
-        return response.json()
-    })
+    .then((response)=>
+        response.json())
+        .then((response)=>{
+        res.json(response)
+   
 
-    .then((respo)=>{
-        res.json(respo)
-    })
+        })
 })
 
 app.get('/shipping',(req,res)=>{
     fetch('https://coinoneglobal.in/teresa_trial/WebOnlineCustomer.asmx/FnGetShippingModeList')
-    .then((response)=>{
-       return response.json()
-    })
-    .then((respo)=>{
-        res.json(respo)
-    })
+    .then((response)=>
+        response.json())
+        .then((response)=>{
+        res.json(response)
+     
+
+        })
 })
 
 
@@ -97,20 +100,18 @@ app.get('/shipping',(req,res)=>{
 app.get('/save',(req,res)=>{
     const {PrmCmpId,PrmBrId,PrmName,PrmKycNo,PrmAddress,PrmCAddress,PrmIsIndian, PrmCountryId, PrmIsAbroad,PrmDueDate,PrmMobileNo,PrmPhoneNo,PrmEmail,PrmOccupation,PrmFunctionId,PrmOtherFunction,PrmDressTypeId,PrmDob,PrmContactTime,PrmSecondName,PrmRefferenceId,PrmShippingId}=req.body
 
-    const url= `https://coinoneglobal.in/teresa_trial/WebOnlineCustomer.asmx/FnSaveCustomer?PrmId=0&PrmCmpId=${req.query.PrmCmpId}&PrmBrId=${req.query.PrmBrId}&PrmName=${req.query.PrmName}&PrmKycNo=${req.query.PrmKycNo}&PrmGender=&PrmPAddress=${req.query.PrmAddress}&PrmCAddress=${req.query.PrmCAddress}&PrmIsIndian=${req.query.PrmIsIndian}&PrmCountryId=${req.query.PrmCountryId}&PrmIsAbroad=${req.query.PrmIsAbroad}&PrmDueDate=${req.query.PrmDueDate}&PrmMobNo=${req.query.PrmMobileNo}&PrmPhoneNo=${req.query.PrmPhoneNo}&PrmEmail=${req.query.PrmEmail}&PrmOccupation=${req.query.PrmOccupation}&PrmFunctionId=${req.query.PrmFunctionId}&PrmOtherFunction=${req.query.PrmOtherFunction}&PrmDressTypeId=${req.query.PrmDressTypeId}&PrmDob=${req.query.PrmDob}&PrmProfileImage=&PrmContactTime=${req.query.PrmContactTime}&PrmIsCourier=false&PrmRemarks=&PrmSecondName=0&PrmRefferenceId=${req.query.PrmRefferenceId}&PrmShippingId=${req.query.PrmShippingId}`
+    
 
-    console.log(url);
-     
+    
     fetch(`https://coinoneglobal.in/teresa_trial/WebOnlineCustomer.asmx/FnSaveCustomer?PrmId=0&PrmCmpId=${req.query.PrmCmpId}&PrmBrId=${req.query.PrmBrId}&PrmName=${req.query.PrmName}&PrmKycNo=${req.query.PrmKycNo}&PrmGender=&PrmPAddress=${req.query.PrmAddress}&PrmCAddress=${req.query.PrmCAddress}&PrmIsIndian=${req.query.PrmIsIndian}&PrmCountryId=${req.query.PrmCountryId}&PrmIsAbroad=${req.query.PrmIsAbroad}&PrmDueDate=${req.query.PrmDueDate}&PrmMobNo=${req.query.PrmMobileNo}&PrmPhoneNo=${req.query.PrmPhoneNo}&PrmEmail=${req.query.PrmEmail}&PrmOccupation=${req.query.PrmOccupation}&PrmFunctionId=${req.query.PrmFunctionId}&PrmOtherFunction=${req.query.PrmOtherFunction}&PrmDressTypeId=${req.query.PrmDressTypeId}&PrmDob=${req.query.PrmDob}&PrmProfileImage=&PrmContactTime=${req.query.PrmContactTime}&PrmIsCourier=false&PrmRemarks=&PrmSecondName=${req.query.PrmSecondName}&PrmRefferenceId=${req.query.PrmRefferenceId}&PrmShippingId=${req.query.PrmShippingId}`)
 
-    // fetch(`https://coinoneglobal.in/teresa_trial/WebOnlineCustomer.asmx/FnSaveCustomer?PrmId=0&PrmCmpId=${req.query.PrmCmpId}&PrmBrId=${req.query.PrmBrId}&PrmName=${req.query.PrmName}&PrmKycNo=${req.query.PrmKycNo}&PrmGender=0&PrmPAddress=${req.query.PrmAddress}&PrmCAddress=0&PrmIsIndian=${req.query.PrmIsIndian}&PrmCountryId=0&PrmIsAbroad=${req.query.PrmIsAbroad}&PrmDueDate=${req.query.PrmDueDate}&PrmMobNo=${req.query.PrmMobileNo}&PrmPhoneNo=0&PrmEmail=${req.query.PrmEmail}&PrmOccupation=${req.query.PrmOccupation}&PrmFunctionId=${req.query.PrmFunctionId}&PrmOtherFunction=${req.query.PrmOtherFunction}&PrmDressTypeId=${req.query.PrmDressTypeId}&PrmDob=${req.query.PrmDob}&PrmProfileImage=0&PrmContactTime=${req.query.PrmContactTime}&PrmIsCourier=${req.query.PrmIsCourier}&PrmRemarks=0`)
-    .then((responsee)=>{
-        return responsee.json()
-    })
-    .then((response)=>{
+    .then((response)=>
+        response.json())
+        .then((response)=>{
         res.json(response)
         console.log(response);
-    })
+
+        })
 })
 
 app.listen(port,()=>{
